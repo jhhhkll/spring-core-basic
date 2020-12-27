cb2dbbe562a3d9448db1b99ab14281fb1bd118a6
손기문 <jhhhkll@gmail.com>
2020-12-27 18:50:21 +0900

    완성

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,9 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import hello.core.logdemo.LogDemoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
